0d0d549f3e75808d8166453aa99d82f9698a2dae
suryadwd <surajdwivedi644@gmail.com>
2026-03-12 20:17:57 +0530

    testing jenkins

--- app/backend/server.js
+++ app/backend/server.js
@@ -6,6 +6,23 @@ import cors from "cors"
 const app = express();
 dotenv.config();
 
+// ================================     observability       =========================
+import client from "prom-client";
+client.collectDefaultMetrics()
+const requestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total HTTP Requests"
+})
+app.get("/", (req,res)=>{
+  requestCounter.inc()
+  res.send("Hello DevOps")
+})
+app.get("/metrics", async (req,res)=>{
+  res.set("Content-Type", client.register.contentType)
+  res.end(await client.register.metrics())
+})
+// ================================     observability       =========================
+
 app.use(
   cors({
     origin: "http://localhost:5173"
@@ -15,9 +32,7 @@ app.use(express.json())
 app.use("/api/products",productRoutes)
 
 
-app.get("/", (req, res) => {
-    return res.send("this is live")
-})
+
 
 app.listen(process.env.PORT , ()=> {
     connectDB()
